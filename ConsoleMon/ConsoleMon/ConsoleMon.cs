9adf28414c9c2721942c2fe8fc57a8e328a2543e
BrendanOssebaar <bqo97@hotmail.com>
2022-05-09 14:38:14 +0200

    Class Opdrachten

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMon
+{
+    class ConsoleMon
+    {
+        public int Health;
+        public int Energy;
+        public string Name;
+        public int Level;
+        public string Environment;
+        public int EXP;
+        public string Type;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMon
+{
+    class Skill
+    {
+        public int Damage;
+        public int EnergyCost;
+        public string Name;
+        public string DamageType;
+
+    }
+}
